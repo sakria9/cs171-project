44b5858a74d69f2cdc92579ee4e2417a07591ed2
sakria9 <i@sustech.moe>
2023-01-09 18:42:28 +0800

    report

--- src/particle.cpp
+++ src/particle.cpp
@@ -679,6 +679,7 @@ void ParticleSystem::MarchCube(float fX, float fY, float fZ, float Scale)
     
 }
 void ParticleSystem::renderSurface(const Scene &scene){
+  buildGrid();
 
   // for(int i=0;i<10;i++)
   //   for(int j=0;j<10;j++)
